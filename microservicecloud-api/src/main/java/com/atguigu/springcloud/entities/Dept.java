d1c061cdb96227f789d4c985e25f851f367f2d30
scottzhangsan <15837192793@163.com>
2019-04-12 13:36:08 +0800

    微服务的第一个生产者

--- microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -8,21 +8,57 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-@Accessors(chain=true)
+
 public class Dept implements Serializable// entity --orm--- db_table
 {
-	private Long 	deptno; // 主键
-	private String 	dname; // 部门名称
-	private String 	db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+	private Long 	deptNo; // 主键
+	private String 	deptName; // 部门名称
+	private String 	dbSource;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
 	
-	public Dept(String dname)
+	
+	
+	public Dept() {
+		super();
+	}
+
+	public Dept(Long deptNo, String deptName, String dbSource) {
+		super();
+		this.deptNo = deptNo;
+		this.deptName = deptName;
+		this.dbSource = dbSource;
+	}
+
+	public Dept(String deptName)
 	{
 		super();
-		this.dname = dname;
+		this.deptName = deptName;
 	}
+
+	public Long getDeptNo() {
+		return deptNo;
+	}
+
+	public void setDeptNo(Long deptNo) {
+		this.deptNo = deptNo;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	public String getDbSource() {
+		return dbSource;
+	}
+
+	public void setDbSource(String dbSource) {
+		this.dbSource = dbSource;
+	}
+	
+	
 	
 	
 }
