c195d3abbb3a3239bca89f47613730e2da8be5b4
scottzhangsan <15837192793@163.com>
2019-04-15 15:30:39 +0800

    添加Hystrix的支持

---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_Hystrix_App.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+/**
+ * @EnableDiscoveryClient和@EnableEurekaClient共同点就是：
+ * 都是能够让注册中心能够发现，扫描到改服务。
+不同点：@EnableEurekaClient只适用于Eureka作为注册中心，
+@EnableDiscoveryClient 可以是其他注册中心。
+ * @author yzhang98
+ *
+ */
+@SpringBootApplication
+@EnableEurekaClient   //服务启动会自动注册进入EureKa服务
+@MapperScan(basePackages={"com.atguigu.springcloud.dao"})
+@EnableCircuitBreaker //对熔断机制的支持
+public class DeptProvider8001_Hystrix_App {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DeptProvider8001_Hystrix_App.class, args);
+	}
+
+}
